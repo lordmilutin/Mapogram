fbc8652afaa7523634f53cffba3db6c3088a4943
Nutic <lordmilutin@gmail.com>
2017-09-01 15:10:12 +0200

    Create login functions

--- app/src/main/java/com/example/nutic/mapogram/LoginActivity.java
+++ app/src/main/java/com/example/nutic/mapogram/LoginActivity.java
@@ -5,9 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
+
+  private String url = "http://mapogram.dejan7.com/api/login";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +39,58 @@ public class LoginActivity extends AppCompatActivity {
         LoginActivity.this.startActivity(myIntent);
       }
     });
+  }
 
-    // Handle login button click
-    final Button loginButton = (Button) findViewById(R.id.loginBtn);
-    loginButton.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View v) {
-        Toast.makeText(getApplicationContext(), "LOGIN ATTEMPT!", Toast.LENGTH_LONG).show();
-      }
-    });
+  public void submitForm(View v) {
+    EditText usernameTf = (EditText) findViewById(R.id.usernameTf);
+    EditText passwordTf = (EditText) findViewById(R.id.passwordTf);
 
+    if (usernameTf.getText().toString().trim().equals("")) {
+      Toast.makeText(getApplicationContext(), "Username is required!", Toast.LENGTH_LONG).show();
+    }
+    else if (passwordTf.getText().toString().trim().equals("")) {
+      Toast.makeText(getApplicationContext(), "Password is required!", Toast.LENGTH_LONG).show();
+    }
+    else {
+      Map<String, String> params = new HashMap();
+
+      params.put("username", usernameTf.getText().toString().trim());
+      params.put("password", passwordTf.getText().toString().trim());
+
+      // Instantiate the RequestQueue.
+      RequestQueue queue = Volley.newRequestQueue(this);
+
+      // Request a string response from the provided URL.
+      JsonObjectRequest jsObjRequest = new JsonObjectRequest
+        (Request.Method.POST, url, new JSONObject(params), new Response.Listener<JSONObject>() {
+
+          @Override
+          public void onResponse(JSONObject response) {
+            String token = "";
+            try {
+              token = response.getString("token");
+            } catch (JSONException e) {
+              e.printStackTrace();
+            }
+            Toast.makeText(getApplicationContext(), "Token:  " + token, Toast.LENGTH_LONG).show();
+          }
+        }, new Response.ErrorListener() {
+
+          @Override
+          public void onErrorResponse(VolleyError error) {
+            String message = "";
+            try {
+              JSONObject errResponse = new JSONObject(new String(error.networkResponse.data));
+              message = errResponse.getString("error");
+            } catch (JSONException e) {
+              e.printStackTrace();
+            }
+            Toast.makeText(getApplicationContext(), "Error: " + message, Toast.LENGTH_LONG).show();
+          }
+        });
+
+      // Add the request to the RequestQueue.
+      queue.add(jsObjRequest);
+    }
   }
 }
--- app/src/main/java/com/example/nutic/mapogram/RegisterActivity.java
+++ app/src/main/java/com/example/nutic/mapogram/RegisterActivity.java
@@ -2,15 +2,28 @@ package com.example.nutic.mapogram;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+
 public class RegisterActivity extends AppCompatActivity {
+
+//  private String url = "http://192.168.1.6:3000/test";
+  private String url = "http://mapogram.dejan7.com/api/users/dejan";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +47,7 @@ public class RegisterActivity extends AppCompatActivity {
         startActivityForResult(intent, 0);
       }
     });
+
   }
 
   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -43,5 +57,48 @@ public class RegisterActivity extends AppCompatActivity {
       final ImageView photoView = (ImageView) findViewById(R.id.photoView);
       photoView.setImageBitmap(imageBitmap);
     }
+  }
+
+  public void submitForm(View v) {
+
+    String jsonData = "{" +
+      "username:\"" + ((EditText) findViewById(R.id.usernameTf)).getText() + "\"," +
+      "first_name:\"" + ((EditText) findViewById(R.id.firstNameTf)).getText() + "\"," +
+      "last_name:\"" + ((EditText) findViewById(R.id.lastNameTf)).getText() + "\"," +
+      "email:\"" + ((EditText) findViewById(R.id.emailTf)).getText() + "\"," +
+      "phone:\"" + ((EditText) findViewById(R.id.phoneNumberTf)).getText() + "\"," +
+      "password:\"" + ((EditText) findViewById(R.id.passwordTf)).getText() + "\"," +
+      "password_confirmation:\"" + ((EditText) findViewById(R.id.passwordConfTf)).getText() + "\"" +
+      "}";
+
+
+    // Instantiate the RequestQueue.
+    RequestQueue queue = Volley.newRequestQueue(this);
+
+    // Request a string response from the provided URL.
+    JsonObjectRequest jsObjRequest = new JsonObjectRequest
+      (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+        @Override
+        public void onResponse(JSONObject response) {
+          System.out.println(response);
+          // Toast.makeText(getApplicationContext(), "Response is: " + response.substring(0, 500) , Toast.LENGTH_LONG).show();
+        }
+      }, new Response.ErrorListener() {
+
+        @Override
+        public void onErrorResponse(VolleyError error) {
+          // TODO Auto-generated method stub
+          Toast.makeText(getApplicationContext(), "That didn't work!", Toast.LENGTH_LONG).show();
+        }
+      });
+
+    // Add the request to the RequestQueue.
+    queue.add(jsObjRequest);
+
+
+    Toast.makeText(getApplicationContext(), "Submiting form!", Toast.LENGTH_LONG).show();
+
+
   }
 }
