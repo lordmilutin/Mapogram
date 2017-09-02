ae7546524ea0fe98d407bc567113bed9e9be5b5b
Nutic <lordmilutin@gmail.com>
2017-09-03 01:34:41 +0200

    Finish AddPhoto

--- app/src/main/java/com/example/nutic/mapogram/AddPhotoActivity.java
+++ app/src/main/java/com/example/nutic/mapogram/AddPhotoActivity.java
@@ -8,11 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,19 +19,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class AddPhotoActivity extends AppCompatActivity {
@@ -84,9 +74,8 @@ public class AddPhotoActivity extends AppCompatActivity {
     params.put("description",    description.getText().toString().trim());
     params.put("categories",    hashtags.getText().toString().trim());
     params.put("img", "data:image/jpeg;base64," + base64);
-    params.put("lng ", "21.899593");
+    params.put("lng", "43.899593");
     params.put("lat", "21.899593");
-
 
     // Request a string response from the provided URL.
     JsonObjectRequest jsObjRequest = new JsonObjectRequest
@@ -94,35 +83,27 @@ public class AddPhotoActivity extends AppCompatActivity {
         @Override
         public void onResponse(JSONObject response) {
           Toast.makeText(getApplicationContext(), "Photo added!", Toast.LENGTH_LONG).show();
-
-          TextView tv = (TextView) findViewById(R.id.textView2);
-          tv.setText(response.toString());
-
-          // Intent myIntent = new Intent(AddPhotoActivity.this, MapsActivity.class);
-          // AddPhotoActivity.this.startActivity(myIntent);
+          Intent myIntent = new Intent(AddPhotoActivity.this, MapsActivity.class);
+          AddPhotoActivity.this.startActivity(myIntent);
         }
       }, new Response.ErrorListener() {
 
         @Override
         public void onErrorResponse(VolleyError error) {
           String message = "";
-//          try {
-//            JSONObject errResponse = new JSONObject(new String(error.networkResponse.data));
-//            message = errResponse.getString("error");
-//          } catch (Exception e) {
-//            e.printStackTrace();
-//          }
-
-          TextView tv = (TextView) findViewById(R.id.textView2);
-          tv.setText(error.toString());
-
+          try {
+            JSONObject errResponse = new JSONObject(new String(error.networkResponse.data));
+            message = errResponse.getString("error");
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
           Toast.makeText(getApplicationContext(), "Error: " + error.toString() , Toast.LENGTH_LONG).show();
         }
       }) {
       @Override
       public Map<String, String> getHeaders() throws AuthFailureError {
         HashMap<String, String> headers = new HashMap<String, String>();
-        headers.put("Content-Type", "application/json");
+        headers.put("Content-Type","application/json");
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         headers.put("Authorization", "Bearer " + settings.getString("token", null));
         return headers;
