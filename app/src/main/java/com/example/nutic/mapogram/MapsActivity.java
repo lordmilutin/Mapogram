4f198e64f74f38815aec517e302c1a38e4e69e4d
DUSAN_DESKTOP_HOME <dusanspasic29@gmail.com>
2017-09-02 21:04:13 +0200

    started bluetooth

--- app/src/main/java/com/example/nutic/mapogram/MapsActivity.java
+++ app/src/main/java/com/example/nutic/mapogram/MapsActivity.java
@@ -1,6 +1,12 @@
 package com.example.nutic.mapogram;
 
 import android.Manifest.permission;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -11,19 +17,17 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request.Method;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -42,13 +46,14 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.HashMap;
 import java.util.Map;
-import org.json.JSONObject;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
-    ConnectionCallbacks, OnConnectionFailedListener, OnMarkerClickListener, LocationListener {
+    ConnectionCallbacks, OnConnectionFailedListener, OnMarkerClickListener, LocationListener,
+    NavigationView.OnNavigationItemSelectedListener {
 
   public static final String PREFS_NAME = "MapogramPrefs";
   private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+  private static final int REQUEST_ENABLE_BT = 2;
   private static final float DEFAULT_ZOOM = 12;
 
   private GoogleMap mMap;
@@ -56,6 +61,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
   private Marker mCurrentMarker;
   private Location mLastLocation;
   private LocationRequest mLocationRequest;
+  private BroadcastReceiver mDeviceDiscoverReceiver;
+  private BluetoothAdapter mBluetoothAdapter;
 
   private String url = "http://mapogram.dejan7.com/api/photos/{location}/{distance}";
 
@@ -75,6 +82,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
           .addApi(LocationServices.API)
           .build();
     }
+
+    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
+
+    IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+
+    mDeviceDiscoverReceiver = new BroadcastReceiver() {
+      public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+          // Discovery has found a device. Get the BluetoothDevice
+          // object and its info from the Intent.
+          BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+          String deviceName = device.getName();
+          Toast.makeText(getApplicationContext(), deviceName, Toast.LENGTH_SHORT).show();
+        }
+      }
+    };
+
+    registerReceiver(mDeviceDiscoverReceiver, filter);
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+
+    unregisterReceiver(mDeviceDiscoverReceiver);
   }
 
   @Override
@@ -86,6 +120,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
   @Override
   protected void onStop() {
     super.onStop();
+    mBluetoothAdapter.cancelDiscovery();
     if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
       mGoogleApiClient.disconnect();
     }
@@ -145,8 +180,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
   @Override
   public void onConnected(@Nullable Bundle bundle) {
     mLocationRequest = new LocationRequest();
-    mLocationRequest.setInterval(1000);
-    mLocationRequest.setFastestInterval(1000);
+    mLocationRequest.setInterval(10000); // intervali osvezavanja
+    mLocationRequest.setFastestInterval(50000);
     mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
     if (ContextCompat.checkSelfPermission(this,
         permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
@@ -193,41 +228,89 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     getUsers(location);
   }
 
-  private void getUsers(final Location location){
+  private void getUsers(final Location location) {
     RequestQueue queue = Volley.newRequestQueue(this);
     // Request a string response from the provided URL.
     String apiUrl = "http://mapogram.dejan7.com/api/location/exchange";
 
-    HashMap<String, String> params = new HashMap<>();
-    params.put("location", String.valueOf(location.getLongitude()) + ", " + String.valueOf(location.getLatitude()));
+//    HashMap<String, String> params = new HashMap<>();
+//    params.put("location",
+//        "21.892018,43.318496");
 
-    final JsonObjectRequest jsObjRequest = new JsonObjectRequest
-        (Method.POST, apiUrl, new JSONObject(params), new Response.Listener<JSONObject>() {
-
+    final StringRequest jsObjRequest = new StringRequest(Method.POST, apiUrl, new Response.Listener<String>() {
           @Override
-          public void onResponse(JSONObject response) {
-            Toast.makeText(getApplicationContext(), "Response: " + response.toString(),
-                Toast.LENGTH_LONG).show();
-          }
-        }, new Response.ErrorListener() {
+          public void onResponse(String response) {
+            Log.d("TAG", "onResponse: " + response);          }
+        }, new ErrorListener() {
+      @Override
+      public void onErrorResponse(VolleyError error) {
+        error.printStackTrace();
+      }
+    }) {
 
-          @Override
-          public void onErrorResponse(VolleyError error) {
-            error.printStackTrace();
-            Toast.makeText(getApplicationContext(), "Error occured", Toast.LENGTH_LONG).show();
-          }
-        }) {
+      @Override
+      protected Map<String, String> getParams() throws AuthFailureError {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("location",
+            "21.892018,43.318496");
+        return params;
+      }
+
       @Override
       public Map<String, String> getHeaders() throws AuthFailureError {
-        HashMap<String, String> header = new HashMap<>();
+        HashMap<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-Type","application/json");
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        header.put("Authorization", settings.getString("token", null));
-        return header;
+        headers.put("Authorization", "Bearer " + settings.getString("token", null));
+        return headers;
       }
     };
 
     // Add the request to the RequestQueue.
     queue.add(jsObjRequest);
+  }
+
+  @Override
+  public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    int id = item.getItemId();
+
+    switch (id) {
+      case R.id.nav_add_friend: {
+        Toast.makeText(this, "ADD FRIEND", Toast.LENGTH_SHORT).show();
+        sendFriendRequestViaBlootooth();
+        return true;
+      }
+      default: {
+        return false;
+      }
+    }
+  }
+
+  private void sendFriendRequestViaBlootooth() {
+    mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    if (mBluetoothAdapter.isEnabled()) {
+      if (mBluetoothAdapter != null) {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+      } else {
+        mBluetoothAdapter.startDiscovery();
+      }
+    }
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode == REQUEST_ENABLE_BT) {
+      if (resultCode == RESULT_OK) {
+        Intent discoverableIntent =
+            new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(discoverableIntent);
+        mBluetoothAdapter.startDiscovery();
+      }
+    }
   }
 }
 
