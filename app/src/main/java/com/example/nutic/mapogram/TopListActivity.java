5115e0aba477d16032a3757e0286a605eddc085d
dejan <dejanstosic7@gmail.com>
2017-09-04 00:09:11 +0200

    map changes

--- app/src/main/java/com/example/nutic/mapogram/TopListActivity.java
+++ app/src/main/java/com/example/nutic/mapogram/TopListActivity.java
@@ -6,11 +6,17 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -24,6 +30,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TopListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -44,8 +52,81 @@ public class TopListActivity extends AppCompatActivity implements NavigationView
     latitude = intent.getDoubleExtra("latitude", 43.321349);
     longitude = intent.getDoubleExtra("longitude", 21.895758);
 
-    loadToplist();
+    //loadToplist();
+      try {
+          loadPeople();
+      } catch (JSONException e) {
+          e.printStackTrace();
+      }
   }
+
+    private void loadPeople() throws JSONException {
+        JsonArrayRequest jsObjRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                final LinearLayout layout = (LinearLayout) findViewById(R.id.topListPeople);
+
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject comment = null;
+                    try {
+                        JSONObject user = response.getJSONObject(i);
+
+                        View commentView = getLayoutInflater().inflate(R.layout.comment, null);
+                        layout.addView(commentView);
+
+                        final ImageView commentPhoto = (ImageView) commentView.findViewById(R.id.commentPhoto);
+                        final TextView commentText = (TextView) commentView.findViewById(R.id.commentText);
+                        final TextView commentAuthorText = (TextView) commentView.findViewById(R.id.commentAuthor);
+                        if (!(user.optString("avatar") == null ||  user.optString("avatar") == "null")) {
+                            DownloadImageTask task = new DownloadImageTask(commentPhoto);
+                            task.execute("http://mapogram.dejan7.com/avatars/" + user.optString("avatar"));
+                        }
+                        final String usernameS = user.getString("username");
+                        commentText.setText(user.optString("total_likes") + " total likes");
+                        commentAuthorText.setText(String.valueOf(i+1) + ". " + user.getString("username") + ":");
+                        Log.e("avatar", user.optString("total_likes"));
+                        commentPhoto.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent = new Intent(TopListActivity.this, ProfileActivity.class);
+                                intent.putExtra("latitude", latitude);
+                                intent.putExtra("longitude", longitude);
+                                intent.putExtra("username", usernameS);
+                                TopListActivity.this.startActivity(intent);
+                            }
+                        });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                String message = "";
+                try {
+                    JSONObject errResponse = new JSONObject(new String(error.networkResponse.data));
+                    message = errResponse.getString("error");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(getApplicationContext(), "Error: " + message, Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Accept", "application/json");
+                SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                headers.put("Authorization", "Bearer " + settings.getString("token", null));
+                return headers;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsObjRequest);
+    }
 
 
   private void loadToplist() {
